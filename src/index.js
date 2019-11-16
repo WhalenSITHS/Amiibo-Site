1d966cd595f9354bf1fe07756ce4ffb970769395
Michael Whalen <michael.whalen@sitechhs.com>
2019-11-16 16:10:29 -0500

    init webpack

---
+++ src/index.js
@@ -0,0 +1,34 @@
+const path = require("path");
+const hbs = require("hbs");
+const express = require("express");
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+//define paths for express configs
+const publicDirectoryPath = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates/views");
+const partialsPath = path.join(__dirname, "../templates/partials");
+
+//setup handlebars engine and views location
+
+app.set("view engine", "hbs");
+app.set("views", viewsPath);
+hbs.registerPartials(partialsPath);
+
+//Setup static directory to serve
+app.use(express.static(publicDirectoryPath));
+
+app.get("", async (req, res) => {
+  try {
+    res.render("index", {
+      title: "Express Amiibo Application"
+    });
+  } catch {
+    res.status(500).send();
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Listening on ${port}`);
+});
---
+++ webpack.config.js
@@ -0,0 +1,11 @@
+const path = require("path");
+module.exports = {
+  entry: "./src/bundles/bundle.js",
+  output: {
+    filename: "app.js",
+    path: path.resolve(__dirname, "public/js")
+  },
+  devServer: {
+    contentBase: "./dist"
+  }
+};
